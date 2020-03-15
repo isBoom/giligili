f40ffd7b4ac74dd9318ba80278a72bed0356023b
isBoom <1520285660@qq.com>
2020-03-15 16:23:31 +0800

    扁平数据转二维

--- service/video_comment_service.go
+++ service/video_comment_service.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"singo/model"
 	"singo/serializer"
+	"time"
 )
 
 type VideoCommentService struct {
@@ -12,7 +13,10 @@ type VideoCommentService struct {
 	ParentId uint   `json:"parentId" form:"parentId"`
 }
 type Comments struct {
-	ID uint
+	ID        uint            `json:"id" form:"id"`
+	UserId    uint            `json:"-" form:"-"`
+	Users     serializer.User `json:"users" form:"users"`
+	CreatedAt time.Time       `json:"createdAt" form:"createdAt"`
 	VideoCommentService
 	Child []Comments `json:"child" form:"child"`
 }
@@ -54,18 +58,49 @@ func (s *VideoCommentService) Add(user *model.User) serializer.Response {
 }
 
 func (s *VideoCommentService) Get(c *gin.Context) serializer.Response {
-	mods := []Comments{}
-	if err := model.DB.Model(&model.Comment{}).Where("video_id = ? and parent_id = 0", c.Param("id")).Find(&mods).Error; err != nil {
+	users := make([]serializer.User, 0)
+	mods := make([]Comments, 0)
+	res := make([]Comments, 0)
+	mapUser := make(map[uint]*serializer.User)
+
+	//指示最高级评论
+	mapFather := make(map[uint]uint)
+	//指示最高级评论所在切片下标
+	mapIndex := make(map[uint]uint)
+	if err := model.DB.Model(&model.Comment{}).Where("video_id = ?", c.Param("id")).Find(&mods).Error; err != nil {
 		return serializer.Response{
 			Code:  5001,
 			Msg:   "获取评论失败",
 			Error: err.Error(),
 		}
 	}
-	for i, mod := range mods {
-		temp := []Comments{}
-		model.DB.Where("video_id = ? and parent_id = ?", mod.VideoId, mod.ID).Find(&temp)
-		mods[i].Child = temp
+	if err := model.DB.Model(&serializer.User{}).Select("DISTINCT users.id,users.user_name,users.nickname,users.avatar").Joins("left JOIN comments on users.id = comments.user_id ").Where("video_id = ?", c.Param("id")).Find(&users).Error; err != nil {
+		return serializer.Response{
+			Code:  5001,
+			Msg:   "获取评论失败",
+			Error: err.Error(),
+		}
 	}
-	return serializer.Response{Data: mods}
+	for i, mod := range users {
+		mapUser[mod.ID] = &users[i]
+	}
+	//实现扁平数据二维化
+	for _, mod := range mods {
+		mod.Users = *mapUser[mod.UserId]
+		if mod.ParentId == 0 {
+			mapIndex[mod.ID] = uint(len(res))
+			res = append(res, mod)
+		} else {
+			if mapFather[mod.ParentId] == 0 {
+				//此时为二级评论
+				mapFather[mod.ID] = mod.ParentId
+			} else {
+				//n级评论
+				mapFather[mod.ID] = mapFather[mod.ParentId]
+			}
+			//最高级评论添加二级到n级子评论
+			res[mapIndex[mapFather[mod.ID]]].Child = append(res[mapIndex[mapFather[mod.ID]]].Child, mod)
+		}
+	}
+	return serializer.Response{Data: res}
 }
